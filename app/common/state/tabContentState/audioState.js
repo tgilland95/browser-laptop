0263bab00a7dcb260424f3d1ad741d1a1073ca48
Cezar Augusto <cezaraugusto@outlook.it>
2017-09-11 06:31:15 -0300

    add new state helpers for the audible tab (tabs w/ audio indicator) Auditors: @nejczdovc Test plan: npm run test -- --grep="audioState unit tests"

--- app/common/state/tabContentState/audioState.js
+++ app/common/state/tabContentState/audioState.js
@@ -5,12 +5,64 @@
 // State helpers
 const frameStateUtil = require('../../../../js/state/frameStateUtil')
 
+// Utils
+const {isEntryIntersected} = require('../../../../app/renderer/lib/observerUtil')
+
 module.exports.canPlayAudio = (state, frameKey) => {
   const frame = frameStateUtil.getFrameByKey(state, frameKey)
 
   if (frame == null) {
+    if (process.env.NODE_ENV !== 'test') {
+      console.error('Unable to find frame for canPlayAudio method')
+    }
     return false
   }
 
   return frame.get('audioPlaybackActive') || frame.get('audioMuted')
+}
+
+module.exports.isAudioMuted = (state, frameKey) => {
+  const frame = frameStateUtil.getFrameByKey(state, frameKey)
+
+  if (frame == null) {
+    if (process.env.NODE_ENV !== 'test') {
+      console.error('Unable to find frame for isAudioMuted method')
+    }
+    return false
+  }
+
+  const tabCanPlayAudio = module.exports.canPlayAudio(state, frameKey)
+  return tabCanPlayAudio && frame.get('audioMuted')
+}
+
+module.exports.showAudioIcon = (state, frameKey) => {
+  const frame = frameStateUtil.getFrameByKey(state, frameKey)
+
+  if (frame == null) {
+    if (process.env.NODE_ENV !== 'test') {
+      console.error('Unable to find frame for showAudioIcon method')
+    }
+    return false
+  }
+
+  return (
+    !isEntryIntersected(state, 'tabs') &&
+    module.exports.canPlayAudio(state, frameKey)
+  )
+}
+
+module.exports.showAudioTopBorder = (state, frameKey, isPinned) => {
+  const frame = frameStateUtil.getFrameByKey(state, frameKey)
+
+  if (frame == null) {
+    if (process.env.NODE_ENV !== 'test') {
+      console.error('Unable to find frame for showAudioTopBorder method')
+    }
+    return false
+  }
+
+  return (
+    module.exports.canPlayAudio(state, frameKey) &&
+    (isEntryIntersected(state, 'tabs') || isPinned)
+  )
 }
